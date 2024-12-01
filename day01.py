6e7abac278756f649e2bb748964728e79ed4032d
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2024-12-01 08:43:28 +0100

    Day 1

--- day01.py
+++ day01.py
@@ -1,3 +1,19 @@
+from collections import Counter
+
 from aoc import get_input
 
-print(get_input(day=1))
+
+rows = [(int(id) for id in row.split())
+        for row in get_input(day=1)]
+l1, l2 = (list(l) for l in zip(*rows))
+
+l1.sort()
+l2.sort()
+
+# 1-1
+total = sum(abs(id2 - id1) for id1, id2 in zip(l1, l2))
+print(total)
+
+# 1-2
+counts = Counter(l2)
+print(sum(v1 * counts.get(v1, 0) for v1 in l1))
