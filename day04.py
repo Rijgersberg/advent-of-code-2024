ff42f9e24223a6d0222d1c2560929b4eec0b9735
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2024-12-04 08:03:01 +0100

    Day 4

---
+++ day04.py
@@ -0,0 +1,56 @@
+import re
+
+from aoc import get_input
+
+
+puzzle = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".splitlines()
+
+puzzle = get_input(day=4)
+
+# 4-1
+def search(puzzle, word, r, c, found, direction):
+    if not (0 <= r < len(puzzle) and 0 <= c < len(puzzle[0])):
+        return False
+    
+    target = word[len(found)]
+    
+    if puzzle[r][c] == target:
+        if found + target == word:
+            return True
+        else:
+            r_n = r + direction[0]
+            c_n = c + direction[1]
+            return search(puzzle, word, r_n, c_n, found+target, direction)
+
+    else:
+        return False
+
+
+total = 0
+for r in range(len(puzzle)):
+    for c in range(len(puzzle[0])):
+        for dr in (-1, 0, 1):
+            for dc in (-1, 0, 1):
+                if (dr, dc) != (0, 0):
+                    total += search(puzzle, "XMAS", r, c, '', (dr, dc))
+print(total)
+
+# 4-2
+crosses = ['M.M\n.A.\nS.S', 'M.S\n.A.\nM.S', 'S.M\n.A.\nS.M', 'S.S\n.A.\nM.M']
+total = 0
+for r in range(1, len(puzzle)-1):
+    for c in range(1, len(puzzle[0])-1):
+        candidate = f'{puzzle[r-1][c-1]}.{puzzle[r-1][c+1]}\n.{puzzle[r][c]}.\n{puzzle[r+1][c-1]}.{puzzle[r+1][c+1]}'
+        candidate = re.sub(r'[^MAS\n]', '.', candidate)
+        if candidate in crosses:
+            total += 1
+print(total)
