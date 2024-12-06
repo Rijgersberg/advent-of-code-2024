0e0a300ccef467889be82bb5fee2e78a6661106d
Edwin Rijgersberg <edwin@edwinrijgersberg.nl>
2024-12-06 11:51:15 +0100

    Cleanup

--- day06.py
+++ day06.py
@@ -1,53 +1,36 @@
-from copy import deepcopy
-from collections import defaultdict
-
 from tqdm import tqdm
 
 from aoc import get_input
 
 
-puzzle = """....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...""".splitlines()
-
 puzzle = get_input(day=6)
 
-map = {}
+map = set()
 for c, line in enumerate(reversed(puzzle)):
     for r, char in enumerate(line):
         if char == '#':
-            map[complex(real=r, imag=c)] = char
+            map.add(complex(real=r, imag=c))
         elif char == '^':
             start_pos = complex(real=r, imag=c)
 
-start_direction = 0 + 1j
 R, C = len(puzzle), len(puzzle[0])
+start_direction = 0 + 1j  # up
 
 def play(map, pos, direction):
-    visited = {pos}
-    state = {(pos, direction)}
+    history = {(pos, direction)}
 
     pos += direction
-
     while 0 <= pos.imag < R and 0 <= pos.real < C:
         if (pos, direction) in state:
             return 'loop'
         
-        visited.add(pos)
-        state.add((pos, direction))
+        history.add((pos, direction))
         
-        if map.get(pos + direction, '') == '#':
+        if pos + direction in map:
             direction *= 0 - 1j  # rotate 90 degrees right
         else:
             pos += direction
-    return visited
+    return {pos for pos, _ in history}
 
 # 6-1
 print(len(play(map, start_pos, start_direction)))
@@ -56,11 +39,11 @@ print(len(play(map, start_pos, start_direction)))
 loops = 0
 for r in tqdm(range(R)):
     for c in range(C):
-        new_map = deepcopy(map)
+        new_map = map.copy()
         pos = complex(real=c, imag=r)
 
-        if not new_map.get(pos, '') == '#' and not pos == start_pos:
-            new_map[pos] = '#'
+        if not pos in map and not pos == start_pos:
+            new_map.add(pos)
             result = play(new_map, start_pos, start_direction)
             if result == 'loop':
                 loops += 1
